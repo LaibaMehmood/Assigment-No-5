0dd6fa94dc0744efbc17927543494f4529e7c2c5
LAIBAMEHMOOD <laibamehmood033@gmaile.com>
2024-03-02 12:56:21 +0500

    first commit

---
+++ FP.js
@@ -0,0 +1,3 @@
+var FamousPersonName = "Albert Einstein";
+var message = "Once said, \"A person who never made a mistake never tried to learn";
+console.log(FamousPersonName, message);
---
+++ FP.ts
@@ -0,0 +1,5 @@
+let FamousPersonName = "Albert Einstein";
+
+let message = "Once said, \"A person who never made a mistake never tried to learn";
+
+console.log(FamousPersonName, message);
